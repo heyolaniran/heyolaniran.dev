4c4371e0a2ad30aac96a0d0ef5d174d8e287d13f
heyolaniran <olaniran.abd@gmail.com>
2024-06-12 15:29:14 +0100

    feat: hero section and navbar initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Hero from '@/components/Hero'
+import { FloatingNav } from '@/components/ui/FloatingNavBar'
 import Image from 'next/image'
+import { FaHome } from 'react-icons/fa'
 
 export default function Home() {
   return (
-  <main className='relative '>
-      Hey Olaniran ! 
+  <main className='relative bg-black-100 flex justify-center flex-col items-center overflow-hidden
+  sm:px-10 px-5'>
+
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home' , link : '/' , icon: <FaHome/>}
+        ]} />
+        
+        <Hero />
+      </div>
+      
   </main>
   )
 }
